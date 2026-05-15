6875628a54f350ac242275ba72be2ff19370fcca
killaboyjuice <iamjuicekidodc@gmail.com>
2026-05-15 10:47:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackBuildConfig } from "@lovable.dev/vite-tanstack-config";
-import path from "node:path";
 
 export default defineConfig({
   ...tanstackBuildConfig,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
 });
