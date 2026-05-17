d8e697ec10657b47069496304b4fe4817d3b9641
killaboyjuice <iamjuicekidodc@gmail.com>
2026-05-17 11:49:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-  define: {
-    'process.env': {},
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  ssr: {
-    noExternal: [
-      '@tanstack/start', 
-      '@tanstack/react-router', 
-      '@tanstack/start-storage-context'
-    ],
+  server: {
+    host: "::",
+    port: 8080,
   },
 });
-
